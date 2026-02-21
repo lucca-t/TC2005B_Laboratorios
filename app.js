36f81f7a680014303b4e7023095b0bc7ca2dfeea
lucca-t <ninja.spinji@gmail.com>
2026-02-21 14:48:50 -0600

    feat(lab8): added the leetcode problem coin change

--- app.js
+++ app.js
@@ -16,6 +16,35 @@ function calcularPromedio(nums) {
 
 let numslist = [3,5,1,2,4,5,6,7];
 
+function escribirTexto(texto) {
+
+    fs.writeFileSync("escribe.txt", texto);
+    console.log("Escribi texto");
+}
+
+function coinChange(coins, amount) {
+    /**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+    // bottom up dp
+    // amount + 1 indica un valor no posible
+    const dp = new Array(amount + 1).fill(amount + 1);
+    dp[0] = 0;
+
+    for (let i = 1; i <= amount; i++) { // Checar cada cantidad de 1 al amt
+        for (const coin of coins) { // Usar cada moneda
+            if (i - coin >= 0) {
+                dp[i] = Math.min(dp[i], 1 + dp[i - coin]); // 
+            }
+        }
+    }
+    // Regresa la cantidad minima de monedas, o - 1 si no es posible 
+    return dp[amount] > amount ? -1 : dp[amount];
+
+}
+
 // Muestra mi index.html
 const server = http.createServer( (request, response) => {
     //console.log(request);
@@ -41,6 +70,14 @@ const server = http.createServer( (request, response) => {
 
 console.log(calcularPromedio(numslist));
 
+console.log("Escribiendo texto");
+escribirTexto("Este es el string que se esta escribiendo.");
+
+let coins = [1,2,5];
+let amount = 11;
+let change = coinChange(coins,amount);
+
+console.log(`Coins list = ${coins}, Amount = ${amount}, Min Coins = ${change}`);
 
 server.listen(3000);
 
