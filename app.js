ddf8f80717c87bb4eb444b63d24a9b5d39269478
lucca-t <ninja.spinji@gmail.com>
2026-03-01 15:47:15 -0600

    feat(lab11): added the Succession info based off of the LoL example in class.

--- app.js
+++ app.js
@@ -2,23 +2,38 @@ const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const rutas_personajes = require('./routes/personajes.routes');
+const rutas_quotes = require('./routes/quotes.routes');
+
 app.use('/personajes', rutas_personajes);
+app.use('/quotes', rutas_quotes);
 
 app.get('/', (request, response, next) => {
-    response.send(`
+    response.redirect('/personajes');
+});
+
+app.use((request, response, next) => {
+    response.status(404).send(`
+        <!DOCTYPE html>
         <html>
-            <body>
-                <a href="/personajes">Personajes</a>
-            </body>
+        <head>
+            <meta charset="utf-8">
+            <title>404</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
+        </head>
+        <body>
+        <section class="section">
+            <div class="container has-text-centered">
+                <h1 class="title">404 - Not Found</h1>
+                <p>The route you requested does not exist.</p>
+                <a class="button is-link mt-4" href="/personajes">Go Home</a>
+            </div>
+        </section>
+        </body>
         </html>
     `);
-    
-})
-app.use((request, response, next) => {
-    response.status(404).send("La ruta no existe");
-})
+});
 
 app.listen(3000);
