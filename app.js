7768747ca08b498dc2ce74c9def3228bca2fdcd7
lucca-t <ninja.spinji@gmail.com>
2026-03-03 12:25:42 -0600

    feat(lab15): added the basics for the login screen

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const session = require('express-session');
+app.use(session({
+    secret: 'mi string secreto que debe ser un string aleatorio muy largo, no como éste', 
+    resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
+    saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
+}));
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -15,6 +22,9 @@ const rutas_quotes = require('./routes/quotes.routes');
 
 app.use('/personajes', rutas_personajes);
 app.use('/quotes', rutas_quotes);
+
+const rutas_usuarios = require('./routes/users.routes');
+app.use('/users', rutas_usuarios);
 
 app.get('/', (request, response, next) => {
     response.redirect('/personajes');
---
+++ routes/users.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const usersController = require('../controller/users.controller');
+
+router.get('/login', usersController.get_login);
+router.post('/login', usersController.post_login);
+router.get('/logout', usersController.get_logout);
+
+
+
+module.exports = router;
