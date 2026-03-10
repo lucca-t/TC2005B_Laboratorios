cf29f0f99ebf5d1e92905d59de698b3ce1fe1478
lucca-t <ninja.spinji@gmail.com>
2026-03-09 23:12:28 -0600

    feat(lab18): added is-auth.js and implemented

--- routes/personajes.routes.js
+++ routes/personajes.routes.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const isAuth = require('../util/is-auth');
 const personajesController = require('../controller/personajes.controller');
 
-
-
-router.get('/new', personajesController.get_add);
-router.post('/new', personajesController.post_add);
-router.get('/search', personajesController.get_search);
-router.get('/old', personajesController.get_old);
-router.get('/:id/edit', personajesController.get_edit);
-router.post('/:id/edit', personajesController.post_edit);
-router.get('/', personajesController.get_list);
+router.get('/new', isAuth, personajesController.get_add);
+router.post('/new', isAuth, personajesController.post_add);
+router.get('/search', isAuth, personajesController.get_search);
+router.get('/old', isAuth, personajesController.get_old);
+router.get('/:id/edit', isAuth, personajesController.get_edit);
+router.post('/:id/edit', isAuth, personajesController.post_edit);
+router.get('/', isAuth, personajesController.get_list);
 
 
 module.exports = router;
--- routes/quotes.routes.js
+++ routes/quotes.routes.js
@@ -2,18 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const quotesController = require('../controller/quotes.controller');
+const isAuth = require('../util/is-auth');
 
-// Show all quotes
-router.get('/', quotesController.get_quotes);
 
-// About page
-router.get('/about', quotesController.get_about);
-
-// New Quote Form
-router.get('/new', quotesController.get_add);
-
-// Add new quote
-router.post('/new', quotesController.post_add);
+router.get('/', isAuth, quotesController.get_quotes);
+router.get('/about', isAuth, quotesController.get_about);
+router.get('/new', isAuth, quotesController.get_add);
+router.post('/new', isAuth, quotesController.post_add);
 
 
 module.exports = router;
